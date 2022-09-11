0ce627aacb87a065086714531029fbbd6b791b9d
Dafinci01 <Davehacker38@gmail.com>
2022-09-11 11:06:07 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,13 @@
 /**
  * main - Prints "Programming is like building a multilingual puzzle"
  *
- * Return: Always 0
+ *
+ *Return: Always 0
+ *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"\);
+	puts("\"Programming is like building a multillingual puzzle");
 	return (0);
+
 }
